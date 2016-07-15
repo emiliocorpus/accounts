5d2458881b9cba2efbfb5094d5933086ed6df687
emiliocorpus <emiliocorpus@gmail.com>
2016-07-15 13:47:06 -0700

    adds create record function with error message

--- app/assets/javascripts/components/records/record_form.jsx
+++ app/assets/javascripts/components/records/record_form.jsx
@@ -3,7 +3,8 @@ var RecordForm = React.createClass({
 		return {
 			title: '',
 			date: '',
-			amount: ''
+			amount: '',
+			error: ''
 		}
 	},
 	inputChangeHandler : function (event) {
@@ -23,20 +24,27 @@ var RecordForm = React.createClass({
 	},
 	handleSubmit:function(e) {
 		e.preventDefault()
-		$.ajax({
-			url: 'http://localhost:3000/records',
-			type: 'POST',
-			dataType: 'JSON',
-			data: {record: this.state},
-			success: function(record) {
-				this.props.handleNewRecord(record)
-				this.setState(this.getInitialState())
-				console.log("Record was saved!");
-			}.bind(this),
-			error: function() {
-				console.log("Record was unable to persist...");
-			}.bind(this)
-		})
+		if (this.valid()) {
+			$.ajax({
+				url: 'http://localhost:3000/records',
+				type: 'POST',
+				dataType: 'JSON',
+				data: {record: this.state},
+				success: function(record) {
+					this.props.handleNewRecord(record)
+					this.setState(this.getInitialState())
+					console.log("Record was saved!");
+				}.bind(this),
+				error: function() {
+					console.log("Record was unable to persist...");
+				}.bind(this)
+			})
+		}
+		else {
+			this.setState({
+				error: 'Please fill out all fields to submit record.'
+			})
+		}
 	},
 	valid:function() {
 		return (
@@ -44,6 +52,7 @@ var RecordForm = React.createClass({
 		)
 	},
 	render:function() {
+		var error = this.state.error
 		return (
 			<form className="form-inline" onSubmit={this.handleSubmit}>
 				<div className="form-group">
@@ -53,9 +62,12 @@ var RecordForm = React.createClass({
 					<input type="text" className="form-control" placeholder="Title" name="title" value={this.state.title} onChange={this.handleChange}/>
 				</div>
 				<div className="form-group">
-					<input type="number" className="form-control" placeholder="Amount" name="amount" value={this.state.amount} onChange={this.handleChange}/>
+					<input type="number" step="0.01" className="form-control" placeholder="Amount" name="amount" value={this.state.amount} onChange={this.handleChange}/>
 				</div>
-				<button type='submit' className='btn btn-primary' disabled={!this.valid}  >Create Record</button>
+				<button type='submit' className='btn btn-primary'>Create Record</button>
+				<div className="error-message form-group">
+					{error}
+				</div>
 			</form>
 		)
 	}
