fe8e9b1bfb6216437cc755a6e1b28de64235a524
emiliocorpus <emiliocorpus@gmail.com>
2016-08-15 13:58:26 -0700

    edits readme and records

--- app/assets/javascripts/components/records/records.jsx
+++ app/assets/javascripts/components/records/records.jsx
@@ -17,7 +17,6 @@ var Records = React.createClass({
 	},
 	deleteRecord:function(record){
 		var index = this.state.records.indexOf(record)
-		var records = React.addons.update(this.state.records, {$splice: [[index,1]]})
 		this.replaceState({
 			records: records
 		})
